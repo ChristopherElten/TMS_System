1eb6930f24c967fca252f6ec6e5229f7622cd675
ChristopherElten <Chris.Elten@gmail.com>
2015-11-29 15:41:43 -0500

    Chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,10 @@ Rails.application.routes.draw do
   get 'signup'  => 'users#new'
 
 
-
   resources :microposts
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  resources :users
-  resources :microposts
 
   # You can have the root of your site routed with "root"
   # root 'users#index'
